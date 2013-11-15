0fc5b2c8042d6d59a0eee4a619fb4b7993daa253
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2013-11-15 19:42:22 -0200

    Preparing for the tests.

--- setup.py
+++ setup.py
@@ -6,6 +6,20 @@ from setuptools import setup, find_packages
 import os
 import sys
 
+# ============================================================================
+from setuptools.command.test import test as TestCommand
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+# ============================================================================
+
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
@@ -15,7 +29,7 @@ install_requires = [
         'seabird',
         ]
 
-version = '0.5.4'
+version = '0.6'
 
 setup(
     name = 'cotede',
@@ -35,4 +49,7 @@ setup(
     keywords = 'CTD SeaBird QualityControl oceanography hydrography',
     #package_dir = {'': './'},
     include_package_data = True,
+    platforms = ['any'],
+    tests_require = ['pytest'],
+    cmdclass = {'test': PyTest},
 )
