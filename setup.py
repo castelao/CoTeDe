02d2b59c02500e7cf818207f1084ea7c388c5a72
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2013-11-15 19:38:16 -0200

    Improving setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,38 @@
 # -*- coding: utf-8 -*-
 
-#from distribute_setup import use_setuptools
-#use_setuptools()
+from setuptools import setup, find_packages
+#from distutils.core import setup
 
-from setuptools import setup
+import os
+import sys
 
-install_requires = ['numpy>=1.1', 'seabird']
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+install_requires = [
+        'numpy>=1.1',
+        'seabird',
+        ]
+
+version = '0.5.4'
 
 setup(
-    name='cotede',
-    version='0.5.4',
-    author='Guilherme Castelão',
-    author_email='guilherme@castelao.net',
-    packages=['cotede'],
-    url='http://cotede.castelao.net',
-    license='See LICENSE.txt',
-    description='Quality Control of CTD profiles',
-    long_description=open('README.rst').read(),
-    install_requires=install_requires,
-    classifiers=[
+    name = 'cotede',
+    version = version,
+    author = 'Guilherme Castelão',
+    author_email = 'guilherme@castelao.net',
+    packages = ['cotede'],
+    url = 'http://cotede.castelao.net',
+    license = 'See LICENSE.txt',
+    description = 'Quality Control of CTD profiles',
+    long_description = open('README.rst').read(),
+    install_requires = install_requires,
+    classifiers = [
         'Development Status :: 2 - Pre-Alpha',
         'Programming Language :: Python :: 2',
         ],
-    keywords='CTD SeaBird QualityControl oceanography hydrography',
+    keywords = 'CTD SeaBird QualityControl oceanography hydrography',
     #package_dir = {'': './'},
-    include_package_data=True,
+    include_package_data = True,
 )
