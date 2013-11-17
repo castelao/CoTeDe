faf53836c502bfbbf534a24c4f16cfd5920f7acb
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2013-11-17 17:09:24 -0200

    Some stuff moved from test_pqc to test_pqctype

--- tests/test_pqc.py
+++ tests/test_pqc.py
@@ -4,30 +4,19 @@
 
 import numpy as np
 
-def func():
+def func(datafile):
     from seabird import cnv
     import cotede.qc
-    data = cnv.fCNV('./tests/dPIRX010.cnv')
+    data = cnv.fCNV(datafile)
     pqc = cotede.qc.ProfileQC(data, saveauxiliary=True)
     return pqc
 
 
 def test_answer():
-    pqc = func()
+    datafile = "./tests/dPIRX010.cnv"
+    pqc = func(datafile=datafile)
     keys = ['timeS', 'pressure', 'temperature', 'temperature2', 'conductivity',
             'conductivity2', 'potemperature', 'potemperature2', 'salinity',
             'salinity2', 'flag']
-    pqc.keys() == keys
-    assert type(pqc.attributes) == dict
+    assert pqc.keys() == keys
     assert len(pqc.attributes) == 11
-    assert hasattr(pqc, 'input')
-    assert hasattr(pqc, 'flags')
-    assert hasattr(pqc, 'auxiliary')
-    assert type(pqc.flags) == dict
-    for k in pqc.flags.keys():
-        assert type(pqc.flags[k]) == dict
-        for kk in pqc.flags[k].keys():
-            assert (type(pqc.flags[k][kk]) == np.ndarray) or \
-                (type(pqc.flags[k][kk]) == bool)
-            if (type(pqc.flags[k][kk]) == np.ndarray):
-                assert pqc.flags[k][kk].dtype == 'int8'
