ee304755010e5d84bedf3cbd9f75586fb7ac824b
Capt. Dr. Guilherme Castelão <guilherme@castelao.net>
2014-02-26 07:39:02 -0300

    Bugfix #2, using getmaskarray as suggested by Eric.

--- cotede/qc.py
+++ cotede/qc.py
@@ -123,7 +123,8 @@ class ProfileQC(object):
         if 'global_range' in cfg:
             self.flags[v]['global_range'] = np.zeros(self.input[v].shape,
                     dtype='i1')
-            self.flags[v]['global_range'][self.input[v].mask == True] = 9
+            # Flag as 9 any masked input value
+            self.flags[v]['global_range'][ma.getmaskarray(self.input[v])] = 9
             ind = (self.input[v] >= cfg['global_range']['minval']) & \
                     (self.input[v] <= cfg['global_range']['maxval'])
             self.flags[v]['global_range'][np.nonzero(ind)] = 1
@@ -148,7 +149,8 @@ class ProfileQC(object):
             cfg_tmp = cfg['gradient_depthconditional']
             g = gradient(self.input[v])
             flag = np.zeros(g.shape, dtype='i1')
-            flag[self.input[v].mask == True] = 9
+            # Flag as 9 any masked input value
+            flag[ma.getmaskarray(self.input[v])] = 9
             # ---- Shallow zone -----------------
             threshold = cfg_tmp['shallow_max']
             flag[np.nonzero( \
@@ -180,7 +182,8 @@ class ProfileQC(object):
                 self.auxiliary[v]['spike'] = s
 
             flag = np.zeros(s.shape, dtype='i1')
-            flag[self.input[v].mask == True] = 9
+            # Flag as 9 any masked input value
+            flag[ma.getmaskarray(self.input[v])] = 9
             flag[np.nonzero(s > threshold)] = 4
             flag[np.nonzero(s <= threshold)] = 1
             self.flags[v]['spike'] = flag
@@ -189,7 +192,8 @@ class ProfileQC(object):
             cfg_tmp = cfg['spike_depthconditional']
             s = spike(self.input[v])
             flag = np.zeros(s.shape, dtype='i1')
-            flag[self.input[v].mask == True] = 9
+            # Flag as 9 any masked input value
+            flag[ma.getmaskarray(self.input[v])] = 9
             # ---- Shallow zone -----------------
             threshold = cfg_tmp['shallow_max']
             flag[np.nonzero( \
@@ -227,7 +231,8 @@ class ProfileQC(object):
                 self.auxiliary[v]['tukey53H_norm'] = s
 
             flag = np.zeros(s.shape, dtype='i1')
-            flag[self.input[v].mask == True] = 9
+            # Flag as 9 any masked input value
+            flag[ma.getmaskarray(self.input[v])] = 9
             flag[np.nonzero(s > threshold)] = 4
             flag[np.nonzero(s <= threshold)] = 1
             self.flags[v]['tukey53H_norm'] = flag
@@ -252,7 +257,8 @@ class ProfileQC(object):
                 self.auxiliary[v]['step'] = s
 
             flag = np.zeros(s.shape, dtype='i1')
-            flag[self.input[v].mask == True] = 9
+            # Flag as 9 any masked input value
+            flag[ma.getmaskarray(self.input[v])] = 9
 
             flag[np.nonzero(ma.absolute(s) > threshold)] = 4
             flag[np.nonzero(ma.absolute(s) <= threshold)] = 1
@@ -274,7 +280,8 @@ class ProfileQC(object):
                 self.auxiliary[v]['density_step'] = ds
 
             flag = np.zeros(s.shape, dtype='i1')
-            flag[self.input[v].mask == True] = 9
+            # Flag as 9 any masked input value
+            flag[ma.getmaskarray(self.input[v])] = 9
 
             flag[np.nonzero(ds < threshold)] = 3 # I'm not sure to use 3 or 4.
             flag[np.nonzero(ds >= threshold)] = 1
@@ -311,10 +318,12 @@ class ProfileQC(object):
                 for k in woa.keys():
                     self.auxiliary[v][k] = woa[k]
                 self.auxiliary[v]['woa_bias'] = woa_bias
+                self.auxiliary[v]['woa_relbias'] = woa_bias/woa['woa_sd']
 
             self.flags[v]['woa_comparison'] = np.zeros(self.input[v].shape,
                     dtype='i1')
-            self.flags[v]['woa_comparison'][self.input[v].mask==True] = 9
+            # Flag as 9 any masked input value
+            self.flags[v]['woa_comparison'][ma.getmaskarray(self.input[v])] = 9
             ind = woa_bias/woa['woa_sd'] <= \
                     cfg['woa_comparison']['sigma_threshold']
             self.flags[v]['woa_comparison'][np.nonzero(ind)] = 1
