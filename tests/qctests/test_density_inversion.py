8f0368d2accae01306c7d7d873be554a3ef19802
Gui <guilherme@castelao.net>
2015-07-03 11:23:57 -0700

    Prototype of test_density_inversion
    Don't test much at this point.

---
+++ tests/qctests/test_density_inversion.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Check density inversion QC test
+"""
+
+from numpy import ma
+
+from cotede.qctests import density_inversion
+
+
+def test():
+    dummy_data = {
+        'PRES': ma.masked_array([0.0, 100, 5000]),
+        'TEMP': ma.masked_array([25.18, 19.73, 2.13]),
+        'PSAL': ma.masked_array([36.00, 34.74, 34.66])
+        }
+
+    cfg = {
+            'threshold': -0.03,
+            'flag_good': 1,
+            'flag_bad': 4
+            }
+
+    f, x = density_inversion(dummy_data, cfg, saveaux=True)
+
+    assert (f == [0,4,4]).all()
