085887337f90317817ff07f1c095ddb7e126e803
Gui <guilherme@castelao.net>
2015-07-15 16:30:49 -0700

    Improving and cleanning test_profilescollection.py

--- tests/test_profilescollection.py
+++ tests/test_profilescollection.py
@@ -1,34 +1,34 @@
-
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from cotede.utils.supportdata import download_testdata
 
 from cotede.utils.profilescollection import process_profiles_serial
 from cotede.utils.profilescollection import process_profiles
-from cotede.utils import ProfilesQCCollection
 
 
-def create_input_list():
-    """
-    """
-    datalist = ["dPIRX010.cnv", "PIRA001.cnv", "dPIRX003.cnv"]
-    return [download_testdata(f) for f in datalist]
+datalist = ["dPIRX010.cnv", "PIRA001.cnv", "dPIRX003.cnv"]
+INPUTFILES = [download_testdata(f) for f in datalist]
+
+
+def check_profiles(profiles):
+    assert len(INPUTFILES) == len(profiles)
+    for p in profiles:
+        assert hasattr(p, 'keys')
+        assert len(p.keys()) > 0
+        assert hasattr(p, 'flags')
+        assert len(p.flags.keys()) > 0
 
 
 def test_process_profiles_serial():
-    inputfiles = create_input_list()
-    profiles = process_profiles_serial(inputfiles, saveauxiliary=False)
-    profiles = process_profiles_serial(inputfiles, saveauxiliary=True)
+    profiles = process_profiles_serial(INPUTFILES, saveauxiliary=False)
+    check_profiles(profiles)
+    profiles = process_profiles_serial(INPUTFILES, saveauxiliary=True)
+    check_profiles(profiles)
 
 
 def test_process_profiles():
-    inputfiles = create_input_list()
-    profiles = process_profiles(inputfiles, saveauxiliary=False)
-    profiles = process_profiles(inputfiles, saveauxiliary=True)
-
-
-def test_ProfilesQCCollection():
-    inputfiles = create_input_list()
-    inputdir = os.path.dirname(inputfiles[0])
-    profiles = ProfilesQCCollection(inputdir, saveauxiliary=False)
-    profiles = ProfilesQCCollection(inputdir, saveauxiliary=True)
+    profiles = process_profiles(INPUTFILES, saveauxiliary=False)
+    check_profiles(profiles)
+    profiles = process_profiles(INPUTFILES, saveauxiliary=True)
+    check_profiles(profiles)
