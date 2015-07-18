c4472cd34d7699c49e0756f5d1a1038e5553f09d
Gui <guilherme@castelao.net>
2015-07-18 16:41:49 -0700

    Creating test_rank_files()

--- tests/test_anomaly_detection.py
+++ tests/test_anomaly_detection.py
@@ -4,10 +4,18 @@
 """ Check density inversion QC test
 """
 
+import tempfile
+import shutil
 
 import numpy as np
 
+from cotede.utils.supportdata import download_testdata
 from cotede.anomaly_detection import split_data_groups
+from cotede.anomaly_detection import rank_files
+
+
+datalist = ["dPIRX010.cnv", "PIRA001.cnv", "dPIRX003.cnv"]
+INPUTFILES = [download_testdata(f) for f in datalist]
 
 
 def test_split_data_groups():
@@ -21,3 +29,17 @@ def test_split_data_groups():
         indices[k].size == N
         assert indices[k].any(), "%s are all True" % k
         assert (~indices[k]).any(), "%s are all False" % k
+
+
+def test_rank_files():
+    try:
+        tmpdir = tempfile.mkdtemp()
+        for f in INPUTFILES:
+            shutil.copy(f, tmpdir)
+
+        output = rank_files(tmpdir, 'TEMP', cfg='cotede')
+    finally:
+        shutil.rmtree(tmpdir)
+
+    assert type(output) is list
+    assert output == ['dPIRX010.cnv', 'dPIRX003.cnv', 'PIRA001.cnv']
