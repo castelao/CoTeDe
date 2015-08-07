9a1767ecf4e524e9f98f179ac69da8f217adeec4
Gui <guilherme@castelao.net>
2015-08-06 20:03:07 -0700

    Improving test_estimate_anomaly_pandas()

--- tests/anomaly_detection/test_estimate_anomaly.py
+++ tests/anomaly_detection/test_estimate_anomaly.py
@@ -49,15 +49,23 @@ def test_estimate_anomaly_dict():
 
 def test_estimate_anomaly_maskedarray():
     features = {'spike': ma.MaskedArray([0, 1, -.5], mask=[False, True, False]),
-                'tukey53H_norm': [-1, 0, 2]
+                'tukey53H_norm': ma.MaskedArray([-1, 0, 2], mask=[False, True, False]),
                 }
     output = estimate_anomaly(features, dummy_params)
 
     assert len(output) == len(features[features.keys()[0]])
     assert type(output) == ma.MaskedArray
+    # If all features for one measurement are masked, the estimated probability
+    #   will be masked, like in the second position of this example
+    assert (output.mask == [False, True, False]).all()
+
+    features['spike'].mask[1] = False
+    output = estimate_anomaly(features, dummy_params)
+
+    assert len(output) == len(features[features.keys()[0]])
+    assert type(output) == ma.MaskedArray
+    # But if is there at least one of features valid, it returns a valid value
+    # Compare this example with the previous where spike[1] is not masked
+    #   anymore.
     assert ~ma.getmaskarray(output).any()
 
-    features = {'spike': ma.MaskedArray([0, 1, -.5], mask=[False, True, False]),
-                'tukey53H_norm': ma.MaskedArray([-1, 0, 2], mask=[False, True, False]),
-                }
-    output = estimate_anomaly(features, dummy_params)
