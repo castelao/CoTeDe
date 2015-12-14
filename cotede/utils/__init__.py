457ffd398a3b2cfb7bf7cd659e178d64df25ef1c
Gui <guilherme@castelao.net>
2015-12-13 18:37:22 -0800

    Migrating to use external pyWOA package instead of utils.woa

--- cotede/utils/__init__.py
+++ cotede/utils/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from cotede.utils.utils import *
-from cotede.utils.woa import *
 from cotede.utils.profilescollection import ProfilesQCCollection
 from cotede.utils.profilescollection import ProfilesQCPandasCollection
 
