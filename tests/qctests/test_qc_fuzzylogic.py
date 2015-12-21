00f88e691c5430ec0b0374689a2b5e6aa1f57c33
Gui <guilherme@castelao.net>
2015-12-21 14:28:00 -0800

    fuzzy preset moved to morello2014.

--- tests/qctests/test_qc_fuzzylogic.py
+++ tests/qctests/test_qc_fuzzylogic.py
@@ -13,10 +13,10 @@ def test():
     """ Only test if run. Must improve this.
     """
     datafile = download_testdata("dPIRX010.cnv")
-    pqc = fProfileQC(datafile, cfg='fuzzy', saveauxiliary=True)
+    pqc = fProfileQC(datafile, cfg='morello2014', saveauxiliary=True)
     # While anomaly detection is limited to spike, gradient and tukey tests it
     #   will always return 0 for the first and last.
-    assert sorted(np.unique(pqc.flags['TEMP']['fuzzylogic'])) == [1,4]
-    assert sorted(np.unique(pqc.flags['TEMP2']['fuzzylogic'])) == [1]
-    assert sorted(np.unique(pqc.flags['PSAL']['fuzzylogic'])) == [1,4]
-    assert sorted(np.unique(pqc.flags['PSAL2']['fuzzylogic'])) == [1]
+    assert sorted(np.unique(pqc.flags['TEMP']['fuzzylogic'])) == [0, 1, 2, 3]
+    assert sorted(np.unique(pqc.flags['TEMP2']['fuzzylogic'])) == [1, 2]
+    assert sorted(np.unique(pqc.flags['PSAL']['fuzzylogic'])) == [0, 1, 2, 3]
+    assert sorted(np.unique(pqc.flags['PSAL2']['fuzzylogic'])) == [1, 2]
