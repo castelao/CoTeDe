5b65bceccc9a207741b7074128f8655494d5c4a6
Gui <guilherme@castelao.net>
2015-12-24 11:58:36 -0800

    Forgot to include global_range into __init__()

--- cotede/qctests/__init__.py
+++ cotede/qctests/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from qctests import *
+from global_range import global_range
 from tukey53H import tukey53H, tukey53H_norm
 from descentPrate import descentPrate
 from location_at_sea import location_at_sea
