7bc545fffb0284e99976d1f1d784aa4aacec080f
Gui <guilherme@castelao.net>
2015-12-27 00:11:32 -0800

    Replicate the common flag to every measurement.
    This is not the most memory efficient approach, but will work for now.

--- cotede/qc.py
+++ cotede/qc.py
@@ -143,6 +143,11 @@ class ProfileQC(object):
 
         self.flags[v] = {}
 
+        # Apply common flag for all points.
+        N = self.input[v].shape
+        for f in self.flags['common']:
+            self.flags[v][f] = self.flags['common'][f] * np.ones(N)
+
         if self.saveauxiliary:
             if v not in self.auxiliary.keys():
                 self.auxiliary[v] = {}
