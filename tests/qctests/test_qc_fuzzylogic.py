66b86f9e8f129dc3783cf556490d9f3a892aa381
Gui <guilherme@castelao.net>
2016-01-01 15:46:01 -0800

    Updating fuzzy logic consistency test.

--- tests/qctests/test_qc_fuzzylogic.py
+++ tests/qctests/test_qc_fuzzylogic.py
@@ -13,10 +13,10 @@ def test():
     """ Only test if run. Must improve this.
     """
     datafile = download_testdata("dPIRX010.cnv")
-    pqc = fProfileQC(datafile, cfg='morello2014', saveauxiliary=True)
+    pqc = fProfileQC(datafile, cfg='morello2014')
     # While anomaly detection is limited to spike, gradient and tukey tests it
     #   will always return 0 for the first and last.
-    assert sorted(np.unique(pqc.flags['TEMP']['morello2014'])) == [0, 1, 2, 3]
-    assert sorted(np.unique(pqc.flags['TEMP2']['morello2014'])) == [1, 2]
-    assert sorted(np.unique(pqc.flags['PSAL']['morello2014'])) == [0, 1, 2, 3]
-    assert sorted(np.unique(pqc.flags['PSAL2']['morello2014'])) == [1, 2]
+    assert sorted(np.unique(pqc.flags['TEMP']['morello2014'])) == [1, 2, 4]
+    assert sorted(np.unique(pqc.flags['TEMP2']['morello2014'])) == [1]
+    assert sorted(np.unique(pqc.flags['PSAL']['morello2014'])) == [1, 2, 4]
+    assert sorted(np.unique(pqc.flags['PSAL2']['morello2014'])) == [1]
