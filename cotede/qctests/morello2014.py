d06b46972a902d01aa70371f7f4ae49b34a94bf8
Gui <guilherme@castelao.net>
2016-01-01 15:40:37 -0800

    Fixing morello2014. It wasn't exactly as the paper.

--- cotede/qctests/morello2014.py
+++ cotede/qctests/morello2014.py
@@ -6,7 +6,6 @@
 
 
 import numpy as np
-#from numpy import ma
 from cotede.fuzzy import fuzzyfy
 
 
@@ -14,21 +13,20 @@ def morello2014(features, cfg):
     """
 
     """
-
     f = fuzzyfy(features, cfg)
 
     ## This is how Timms and Morello defined the Fuzzy Logic approach
-    #flag = np.zeros(self.input[v].shape, dtype='i1')
     #flag = np.zeros(N, dtype='i1')
+    # Flag must be np.array, not a ma.array.
     flag = np.zeros(features[features.keys()[0]].shape, dtype='i1')
 
-    flag[(f['low'] > 0.9)] = 1
     flag[(f['low'] > 0.5) & (f['high'] < 0.3)] = 2
-    # Missing check if threshold was crossed, to flag as 4
+    flag[(f['low'] > 0.9)] = 1
     # Everything else is flagged 3
-    ind = flag == 0
-    for f in features:
-        ind = ind & ~features[f].mask
-    flag[ind] = 3
+    flag[(f['low'] <= 0.5) | (f['high'] >= 0.3)] = 3
+    # Missing check if threshold was crossed, to flag as 4
+    # The thresholds coincide with the end of the ramp for the fuzzy set high,
+    #   hence we can simply
+    flag[(f['high'] == 1.)] = 4
 
     return flag
