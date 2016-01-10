8fc38ef4ca10383129bcf9067dd6602bfc40ca3b
Gui <guilherme@castelao.net>
2016-01-09 23:32:21 -0800

    Forgot to remove bin_spike from qctests.py.

--- cotede/qctests/qctests.py
+++ cotede/qctests/qctests.py
@@ -10,22 +10,3 @@ def step(x):
     y = ma.masked_all(x.shape, dtype=x.dtype)
     y[1:] = ma.diff(x)
     return y
-
-
-def bin_spike(x, l):
-    """
-
-        Dummy way to avoid warnings when x[ini:fin] are all masked.
-        Improve this in the future.
-    """
-    N = len(x)
-    bin = ma.masked_all(N)
-    half_window = l/2
-    for i in range(half_window, N-half_window):
-        ini = max(0, i - half_window)
-        fin = min(N, i + half_window)
-        if ~x[ini:fin].mask.any():
-            bin[i] = x[i] - ma.median(x[ini:fin])
-            #bin_std[i] = (T[ini:fin]).std()
-
-    return bin
