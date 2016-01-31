16410d45dfb2e38f49160990da28d4594b92af73
Gui <guilherme@castelao.net>
2016-01-31 12:50:27 -0800

    Temporary solution for tests not implemented yet.

--- cotede/qctests/qctests.py
+++ cotede/qctests/qctests.py
@@ -10,3 +10,56 @@ def step(x):
     y = ma.masked_all(x.shape, dtype=x.dtype)
     y[1:] = ma.diff(x)
     return y
+
+
+# FIXME, tests to adjust on public version
+def valid_datetime():
+    pass
+
+def frozen_profile():
+    pass
+
+def valid_geolocation():
+    pass
+
+def valid_position():
+    pass
+
+def deepest_pressure():
+    pass
+
+def regional_range():
+    pass
+
+def grey_list():
+    pass
+
+def gradient_depthconditional():
+    pass
+
+def spike_depthconditional():
+    pass
+
+def digit_roll_over():
+    pass
+
+def valid_speed():
+    pass
+
+def gross_sensor_drift():
+    pass
+
+def pressure_increasing():
+    pass
+
+def stuck_value():
+    pass
+
+def platform_identification():
+    pass
+
+def pstep():
+    pass
+
+def constant_value():
+    pass
