fbfa2bcea0f4a53169c995649488b86153042918
Gui <guilherme@castelao.net>
2016-03-30 21:14:19 -0700

    Guarantee compatibility with python 3.5.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'License :: OSI Approved :: BSD License',
         ],
     keywords='CTD TSG SeaBird ARGO Quality Control oceanography hydrography',
