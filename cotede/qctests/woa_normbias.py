b9e6f0c7301657c38ec9a093e8a9d29aa6192c9d
Gui <guilherme@castelao.net>
2016-04-15 17:00:08 -0700

    Creating class WOA_NormBias().

--- cotede/qctests/woa_normbias.py
+++ cotede/qctests/woa_normbias.py
@@ -14,6 +14,8 @@ from numpy import ma
 
 from oceansdb import WOA
 
+FLAG_GOOD = 1
+FLAG_BAD = 3
 
 def woa_normbias(data, v, cfg):
     """
@@ -98,3 +100,92 @@ def woa_normbias(data, v, cfg):
         # self.logger.warn("%s - WOA is not available at this site" %
         # self.name)
         return flag, features
+
+
+class WOA_NormBias(object):
+    def __init__(self, data, varname, cfg):
+        self.data = data
+        self.varname = varname
+        self.cfg = cfg
+
+        self.set_features()
+
+    def keys(self):
+        return self.features.keys() + \
+                ["flag_%s" % f for f in self.flags.keys()]
+
+    def set_features(self):
+
+        if ('LATITUDE' in self.data.keys()) and \
+                ('LONGITUDE' in self.data.keys()):
+                    assert False, "I'm not ready for that"
+
+        elif ('LATITUDE' in self.data.attributes.keys()) and \
+                ('LONGITUDE' in self.data.attributes.keys()):
+                    lat = self.data.attributes['LATITUDE']
+                    lon = self.data.attributes['LONGITUDE']
+
+        if ('PRES' in self.data.keys()):
+            pres = self.data['PRES']
+
+        doy = int(self.data.attributes['datetime'].strftime('%j'))
+
+        db = WOA()
+        if self.varname not in db.keys():
+            vtype = self.varname[:-1]
+        else:
+            vtype = self.varname
+
+        woa = db[vtype].extract(
+                var=['mn', 'sd', 'dd'],
+                doy=doy,
+                depth=pres,
+                lat=lat,
+                lon=lon)
+
+        self.features = {
+                'woa_mean': woa['mn'],
+                'woa_std': woa['sd'],
+                'woa_nsamples': woa['dd']}
+
+        self.features['woa_bias'] = self.data[self.varname] - \
+                self.features['woa_mean']
+        self.features['woa_normbias'] = self.features['woa_bias'] / \
+                self.features['woa_std']
+
+    def test(self):
+
+        # 3 is the possible minimum to estimate the std, but I shold use higher.
+        try:
+            min_samples = cfg['min_samples']
+        except:
+            min_samples = 3
+
+        self.flags = {}
+
+        try:
+            flag_good = self.cfg['flag_good']
+        except:
+            flag_good = FLAG_GOOD
+        try:
+            flag_bad = self.cfg['flag_bad']
+        except:
+            flag_bad = FLAG_BAD
+
+        threshold = self.cfg['threshold']
+        assert (np.size(threshold) == 1) and \
+                (threshold is not None)
+
+        flag = np.zeros(self.data[self.varname].shape, dtype='i1')
+
+        ind = np.nonzero((self.features['woa_nsamples'] >= min_samples) &
+                (np.absolute(self.features['woa_normbias']) <= threshold))
+        flag[ind] = flag_good
+        ind = np.nonzero((self.features['woa_nsamples'] >= min_samples) &
+                (np.absolute(self.features['woa_normbias']) > threshold))
+        flag[ind] = flag_bad
+
+        # Flag as 9 any masked input value
+        flag[ma.getmaskarray(self.data[self.varname])] = 9
+
+        self.flags['woa_normbias'] = flag
