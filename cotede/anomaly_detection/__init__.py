eb1dbdf6bb29f0f628631e927ef8bc8d239bd388
Guilherme Castelão <guilherme@castelao.net>
2016-07-19 21:01:40 -0700

    Fixing some imports.

--- cotede/anomaly_detection/__init__.py
+++ cotede/anomaly_detection/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+from .anomaly_detection import estimate_anomaly
 from anomaly_detection import *
--- cotede/fuzzy/__init__.py
+++ cotede/fuzzy/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-from fuzzy_core import fuzzyfy, fuzzy_uncertainty
+from .fuzzy_core import fuzzyfy, fuzzy_uncertainty
