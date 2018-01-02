BUGFIX #32, forcing NaN on edge data of Spike, Gradient, ...

cotede/qctests/descentPrate.py
@@ -23,5 +23,5 @@ def descentPrate(data):
             "t and p have different sizes"
     dt = ma.diff(data['timeS'])
     dp = ma.diff(data['PRES'])
-    y = ma.append(ma.masked_all(1, dtype='i'), dp/dt)
+    y = ma.append(ma.fix_invalid(np.nan), dp/dt)
     return y

cotede/qctests/gradient.py
@@ -19,9 +19,8 @@ def gradient(x):
         d/dx + d/dy + d/dz,
         but as defined by GTSPP, EuroGOOS and others.
     """
-    y = ma.masked_all_like(x)
+    y = ma.fix_invalid(np.ones_like(x) * np.nan)
     y[1:-1] = np.abs(x[1:-1] - (x[:-2] + x[2:])/2.0)
-
     return y
 
 

cotede/qctests/rate_of_change.py
@@ -15,7 +15,7 @@ from numpy import ma
 
 def rate_of_change(data, v, cfg):
 
-    RoC = ma.masked_all_like(data[v])
+    RoC = ma.fix_invalid(np.ones_like(data[v]) * np.nan)
     RoC[1:] = ma.diff(data[v])
 
     flag = np.zeros(data[v].shape, dtype='i1')

cotede/qctests/tukey53H.py
@@ -27,7 +27,8 @@ def tukey53H(x):
     u3 = ma.masked_all(N)
     u3[1:-1] = 0.25*(u2[:-2] + 2*u2[1:-1] + u2[2:])
 
-    Delta = ma.absolute(x-u3)
+    Delta = ma.fix_invalid(np.ones_like(x) * np.nan)
+    Delta[1:-1] = ma.absolute(x[1:-1]-u3[1:-1])
 
     return Delta
 