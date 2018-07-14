fcf6822224cc61ff34f10c9297df15877c38aa17
Guilherme Castelão <guilherme@castelao.net>
2018-07-13 17:20:48 -0700

    Cleanning test_common.

--- tests/test_common_flag.py
+++ tests/test_common_flag.py
@@ -7,19 +7,12 @@
 import numpy as np
 from cotede.qc import ProfileQC
 
-
-class DummyData(object):
-    def __init__(self):
-        self.attributes = {'LATITUDE': 15, 'LONGITUDE': -38}
-        self.data = {'TEMP': np.array([1,2,3])}
-    def __getitem__(self, key):
-        return self.data[key]
-    def keys(self):
-        return self.data.keys()
+from data import DummyData
 
 
 def test_common_flags():
     profile = DummyData()
+
     cfg = {"main": {"valid_datetime": None, "valid_geolocation": None},
             "TEMP": {}}
     pqc = ProfileQC(profile, cfg=cfg)
@@ -27,7 +20,3 @@ def test_common_flags():
     assert 'common' in pqc.flags
     assert 'valid_datetime' in pqc.flags['TEMP']
     assert pqc.flags['TEMP']['valid_datetime'].shape == profile['TEMP'].shape
-
-
-
-
