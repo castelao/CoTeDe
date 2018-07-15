78d18485bc9736741496ba9e0407e5cdd754b098
Guilherme Castelão <guilherme@castelao.net>
2018-07-14 19:25:23 -0700

    Temporary dummy dataset for tests.
    I need to spend sometime choosing special values that could capture most
    of the tests with the smallest dataset possible.

--- tests/data.py
+++ tests/data.py
@@ -12,9 +12,15 @@ class DummyData(object):
                 'datetime': datetime(2016,6,4),
                 'LATITUDE': 15, 'LONGITUDE': -38}
         self.data = {
-                'PRES': ma.masked_array([0.0, 100, 200, 5000]),
-                'TEMP': ma.masked_array([25.16, 19.73, 16.80, 2.12]),
-                'PSAL': ma.masked_array([32.00, 34.74, 34.66, 35.03])}
+                'PRES': ma.fix_invalid(
+                    [2, 6, 10, 21, 44, 79, 100, 150,
+                        200, 400, 410, 650, 1000, 2000, 5000]),
+                'TEMP': ma.fix_invalid(
+                    [25.32, 25.34, 25.34, 25.31, 24.99, 23.46, 21.85, 17.95,
+                        15.39, 11.08, 6.93, 7.93, 5.71, 3.58, np.nan]),
+                'PSAL': ma.fix_invalid(
+                    [36.49, 36.51, 36.52, 36.53, 36.59, 36.76, 36.81, 36.39,
+                        35.98, 35.30, 35.28, 34.93, 34.86, np.nan, np.nan])}
 
     def __getitem__(self, key):
         return self.data[key]
