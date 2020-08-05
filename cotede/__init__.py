5d69eaf009331f091c2add80d68473d13a5f23f9
Guilherme Castelão <guilherme@castelao.net>
2020-08-04 20:16:56 -0700

    Bump version: 0.21.3 → 0.22.0

--- cotede/__init__.py
+++ cotede/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Guilherme Castelao'
 __email__ = 'guilherme@castelao.net'
-__version__ = '0.21.3'
+__version__ = '0.22.0'
 
 from cotede import qc
 from cotede.qc import ProfileQC, ProfileQCed
