c15eeb1ea048d1e25ee1ec4c9ca5fdf1c0594382
Guilherme Castelão <guilherme@castelao.net>
2020-12-24 16:37:00 -0800

    feat: CTD profile sample data
    dataset.load_ctd() function to load a sample CTD profile for
    demonstration purposes.
    
    Missing the supportdata to load it remotely.

--- cotede/datasets.py
+++ cotede/datasets.py
@@ -12,6 +12,29 @@ except ImportError:
     PANDAS_AVAILABLE = False
 
 
+def load_ctd():
+    filename = os.path.join(cotederc("sampledata"), "dPIRX010.npz")
+    attrs = np.load(filename, allow_pickle=True)["attrs"]
+    attrs = {a[0]: a[1] for a in attrs}
+
+    varnames = np.load(filename, allow_pickle=True)["varnames"]
+    values = np.load(filename, allow_pickle=True)["data"]
+    data = {k: v for k, v in zip(varnames, values)}
+
+    class CoTeDeDataModel(object):
+        def __init__(self, attrs, data):
+            self.attrs = attrs
+            self.data = data
+        def __getitem__(self, key):
+            return self.data[key]
+        def keys(self):
+            return self.data.keys()
+
+    output = CoTeDeDataModel(attrs, data)
+
+    return output
+
+
 def load_water_level(as_frame=False):
     filename = os.path.join(cotederc("sampledata"), "NOS_8764227.npz")
     data = np.load(filename, allow_pickle=True)["water_level"]
