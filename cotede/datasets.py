34ecf95a67dad5e05519539898e16c689f20c9c4
Guilherme Castelão <guilherme@castelao.net>
2020-12-25 13:03:53 -0800

    feat: Use supportdata to download sample data
    The sample data is not shipped within the package to save space. An
    external package named supportdata is used to download those sample
    datasets if requests by the user.

--- cotede/datasets.py
+++ cotede/datasets.py
@@ -11,9 +11,25 @@ try:
 except ImportError:
     PANDAS_AVAILABLE = False
 
+try:
+    from supportdata import download_file
+
+    SUPPORTDATA_AVAILABLE = True
+except ImportError:
+    SUPPORTDATA_AVAILABLE = True
+
 
 def load_ctd():
+    """
+    """
+    DATA_URL = "https://raw.githubusercontent.com/castelao/dummy/main/dPIRX010.npz"
+    DATA_HASH = "16de9b674e7dceb4ac24feedda58a3ab"
+
     filename = os.path.join(cotederc("sampledata"), "dPIRX010.npz")
+    if not os.path.exists(filename):
+        assert SUPPORTDATA_AVAILABLE, "Require package supportdata to download samples"
+        download_file(cotederc("sampledata"), DATA_URL, md5hash=DATA_HASH)
+
     attrs = np.load(filename, allow_pickle=True)["attrs"]
     attrs = {a[0]: a[1] for a in attrs}
 
@@ -36,7 +52,16 @@ def load_ctd():
 
 
 def load_water_level(as_frame=False):
+    """
+    """
+    DATA_URL = "https://raw.githubusercontent.com/castelao/dummy/main/NOS_8764227.npz"
+    DATA_HASH = "7ec673d82e0e361acfc4075fe914dc7d"
+
     filename = os.path.join(cotederc("sampledata"), "NOS_8764227.npz")
+    if not os.path.exists(filename):
+        assert SUPPORTDATA_AVAILABLE, "Require package supportdata to download samples"
+        download_file(cotederc("sampledata"), DATA_URL, md5hash=DATA_HASH)
+
     data = np.load(filename, allow_pickle=True)["water_level"]
     data = {
         "epoch": data[:, 0].astype("f"),
