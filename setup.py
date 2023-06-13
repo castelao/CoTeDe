1f73f33206ae0d177ff0077fb25a7f5b72e6de7a
Gui Castelao <guilherme@castelao.net>
2023-06-12 18:04:15 -0600

    More cleaning

--- setup.py
+++ setup.py
@@ -10,15 +10,7 @@ except ImportError:
     from distutils.core import setup
 
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read().replace('.. :changelog:', '')
-
 setup(
-    version='0.23.8',
-    long_description=readme + '\n\n' + history,
     packages=[
         'cotede',
         'cotede.qctests',
@@ -29,7 +21,5 @@ setup(
     ],
     package_dir = {'cotede':
                    'cotede'},
-    license='3-clause BSD',
     include_package_data=True,
-    zip_safe=False,
 )
